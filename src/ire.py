851c5cc0952dd8aa3f1bcad518cfd1badd90bdd9
Everton Constantino <everton.constantino@mykolab.com>
2016-04-15 18:07:03 -0300

    Cleaning stuff, adding a cleaner way to read files and starting to generate the circles based on the points given. Some weird problem with coordinates right now.

--- src/ire.py
+++ src/ire.py
@@ -1,44 +1,82 @@
+#!/usr/bin/python2
 from osgeo import ogr
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
 
-def draw_points(g):
-    count = g.GetPointCount()
-    i = 0
-    while i < count-1:
-        x1 = g.GetX(i)
-        y1 = g.GetY(i)   
-        x2 = g.GetX(i+1)
-        y2 = g.GetY(i+1)
-        #plt.plot([x1,x2],[y1,y2],'-')
-        i = i + 2
-
 root = './data/gis/'
+roadfile = root + 'estradas_Macacu.shp'
+pointsfile = root + 'Frag_Macacu_pontos.shp'
+
 driver = ogr.GetDriverByName('ESRI Shapefile')
 
-ds = driver.Open(root+'florestotal.shp',0)
+dsroad = driver.Open(roadfile,0)
+dspoints = driver.Open(pointsfile,0)
 
-layer = ds.GetLayer()
-print layer.GetFeatureCount()
-print ds.GetLayerCount()
+def get_geometry(geom):
+    g = []
 
-feat = layer.GetNextFeature()
-while feat:
-    geom = feat.GetGeometryRef()
-    #print 'geom:',geom.GetGeometryName(),geom.GetGeometryCount(),geom.GetPointCount()
     if geom.GetGeometryCount() > 0:
         i = 0
         while i < geom.GetGeometryCount():
             subgeom = geom.GetGeometryRef(i)
-            #print 'subgeom:',i, subgeom.GetGeometryName(),subgeom.GetPointCount()
-            draw_points(subgeom)
+            g.append(subgeom.Clone())
             i = i + 1
     else:
-        draw_points(geom)
-    print feat.DumpReadable()
-    feat.Destroy()
+        g.append(geom.Clone())
+
+    return g        
+
+def read_shape(f):
+    geoms = []
+    layer = f.GetLayer()
     feat = layer.GetNextFeature()
+    while feat:
+        geom = feat.GetGeometryRef()
+        #print 'geom:',geom.GetGeometryName(),geom.GetGeometryCount(),geom.GetPointCount()
+        print feat.DumpReadable()
+        geoms.extend(get_geometry(geom))
+        feat.Destroy()
+        feat = layer.GetNextFeature()
+
+    return geoms
+
+def draw(g,isRoad):
+    count = g.GetPointCount()
+    i = 0
+    if isRoad:
+        while i < count-1:
+            x1 = g.GetX(i)
+            y1 = g.GetY(i)   
+            x2 = g.GetX(i+1)
+            y2 = g.GetY(i+1)
+            plt.plot([x1,x2],[y1,y2],'-')
+            #plt.plot([x1],[y1],'o')
+            print x1,y1
+            #i = i + 1
+            i = i + 2
+    else:            
+        while i < count:
+            x1 = g.GetX(i)
+            y1 = g.GetY(i)   
+            plt.plot([x1],[y1],'o')
+            print x1,y1
+            i = i + 1
+
+points = read_shape(dspoints)
+roads = read_shape(dsroad)
+
+for p in points:
+    draw(p,False)
+
+for r in roads[1:3]:
+    draw(r,True)
+
+#for r in roads:
+#    draw(r,True)
+
+#print len(points)
+#print len(roads)
 
 #plt.plot([0,1,2,3],[1,6,13,22],'-')
 #plt.plot([1,6,13,22],[0,1,2,3],'-')
